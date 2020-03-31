cb5d89d704113bf67ac4ad21b33e511a8c5e7152
marysethomas <marysethomas06@gmail.com>
2020-03-31 15:45:58 -0400

    Update compile_blocks_from_info.m

--- compile_blocks_from_info.m
+++ compile_blocks_from_info.m
@@ -11,9 +11,14 @@
 %
 %  Maryse Thomas - March 2020
 
-%% Create setup variable for files in Info.mat
+%% Load Info
 
-%% Info.mat Table is a variable that stores all recording file information
+path_name = 'D:/Data/2p/VIPvsNDNF_response_stimuli_study'; %Include end slash if you're Carolyn
+cd(path_name)
+load('Info.mat')
+
+%Info.mat Table is a variable that stores all recording file information
+%where each row of Info corresponds to a single recording block
 
 %  Currently, the column order of Info is:
 I = 1; %ignore (0 or 1 allowing user to "hide" some files from analysis)
@@ -32,35 +37,31 @@ VR = 13; %voltage recording (0 for widefield, 1 for 2p)
 SN = 14; %stim name (type of stim presentation in plain text)
 SP = 15; %stim protocol (number corresponding to stim)
 
-%% Look for files that match stim_protocol
-%Later we could update this to also look for other parameters
+%% Compile all blocks unless they are set to "Ignore"
 
-%stim protocol code is:
-%noiseburst=1
-%ReceptiveField=2
-%FM sweep=3
-%SAM = 5
-%widefield=4
-%SAM freq = 6
-code = {'Noiseburst', 'Receptive Field', 'FM sweep', 'SAM', 'Widefield', 'SAM freq'};
-display(['Analyzing ' code{setup.stim_protocol} ' files'])
+%Remove rows that are set to "Ignore"
+ignore = [Info{:,I}]';
+currentInfo = Info(ignore == 0,:);
+
 
 %Find table rows that match stim_protocol
 stims = [Info{:,SP}]';
 matching_stims = stims == setup.stim_protocol;
 currentInfo = Info(matching_stims,:);
 
-%Remove rows that are set to "Ignore"
-ignore = [currentInfo{:,I}]';
-currentInfo = currentInfo(ignore == 0,:);
+
+% Create setup variable that will contain all the necessary information about the block
+
+% 
 
 setup = struct;
 setup.username = ''; %'Carolyn'
-setup.path_name = 'D:/Data/2p/VIPvsNDNF_response_stimuli_study'; %Include end slash if you're Carolyn
-cd(setup.path_name)
-load('Info.mat')
+setup.pathname
 setup = fillSetupFromInfoTable(setup, Info);
 setup.Info = Info;
+
+
+
 
 
         tempFrame_set = {};

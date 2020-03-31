0947692598f60047ae0615574e79d8257172ca1e
marysethomas <marysethomas06@gmail.com>
2020-03-31 16:44:58 -0400

    Update compile_blocks_from_info.m

--- compile_blocks_from_info.m
+++ compile_blocks_from_info.m
@@ -1,49 +1,33 @@
 %% compile_blocks_from_info
+%
 %  Save one compiled 'block.mat' file for each block listed in info.mat
 %  A compiled block contains the 2p and Tosca data for that recording session
+%
+%  Info.mat is a variable that stores all recording file information
+%  where each row of Info corresponds to a single recording block
 %
 %  Goal: If data is missing, the script will still run in order to allow for the
 %  user to look at Tosca data separate from 2p data or vice versa.
 %
 %  Uses the function compile_block and verify_block
 %
-%  Use verify_block to preview the block contents prior to analysis
+%  Use visualize_block to preview the block contents prior to analysis
 %
 %  Maryse Thomas - March 2020
 
-%% Load Info
+%% Load Info.mat
 
-verify = 0; %1 to plot figures of the block immediately, 0 to skip
+visualize = 0; %1 to plot figures of the block immediately, 0 to skip
 
 info_path = 'D:/Data/2p/VIPvsNDNF_response_stimuli_study';
 save_path = 'D:/Data/2p/VIPvsNDNF_response_stimuli_study';
 cd(info_path)
 load('Info.mat')
 
-%Info.mat Table is a variable that stores all recording file information
-%where each row of Info corresponds to a single recording block
-
-%  Currently, the column order of Info is:
-Col.I = 1; %ignore (0 or 1 allowing user to "hide" some files from analysis)
-Col.U = 2; %user name %Not used right now
-Col.M = 3; %mouse name
-Col.P = 4; %path name (path to main folder of mouse's data) %Not used right now
-Col.D = 5; %experiment date
-Col.R = 6; %ROI (which data to consider as coming from the same ROI)
-Col.IS = 7; %imaging set (BOT #s)
-Col.TS = 8; %Tosca session #
-Col.TR = 9; %Tosca run #
-Col.AP = 10; %analysis path name (path to suite2p analysis)
-Col.FR = 11; %frame rate (15 or 30)
-Col.RR = 12; %do you have red cells? 0 or 1 %Not used right now
-Col.VR = 13; %voltage recording (0 for widefield, 1 for 2p)
-Col.SN = 14; %stim name (type of stim presentation in plain text)
-Col.SP = 15; %stim protocol (number corresponding to stim)
-
 %% Compile all blocks unless they are set to "Ignore"
 
 %Remove rows that are set to "Ignore"
-ignore = [Info{:,Col.I}]';
+ignore = [Info{:,1}]';
 currentInfo = Info(ignore == 0,:);
 
 %Loop through all remaining rows
@@ -51,34 +35,41 @@ for i = 1:size(currentInfo,1)
 
     %Create setup variable that will contain all the necessary information about the block
     setup = struct;
-    setup.Info              =   Info;
-    setup.username          =   [currentInfo{i,Col.U}];
-    setup.mousename         =   [currentInfo{i,Col.M}];
-    setup.pathname          =   [currentInfo{i,Col.P}];
-    setup.expt_date         =   [currentInfo{i,Col.D}];
-    setup.ROI               =   [currentInfo{i,Col.R}];
-    setup.imaging_set       =   [currentInfo{i,Col.IS}];
-    setup.Tosca_session     =   [currentInfo{i,Col.TS}];
-    setup.Tosca_run         =   [currentInfo{i,Col.TR}];
-    setup.analysis_name     =   [currentInfo{i,Col.AP}];
-    setup.framerate         =   [currentInfo{i,Col.FR}];
-    setup.run_redcell       =   [currentInfo{i,Col.RR}];
-    setup.voltage_recording =   [currentInfo{i,Col.VR}];
-    setup.stim_name         =   [currentInfo{i,Col.SN}];
-    setup.stim_protocol     =   [currentInfo{i,Col.SP}];
+    setup.Info              =   Info;                   %Record Info for records only
+    setup.username          =   [currentInfo{i,2}];     %part of the path, not every user will have this, okay to leave empty
+    setup.mousename         =   [currentInfo{i,3}];     %part of the path, no underscores
+    setup.pathname          =   [currentInfo{i,4}];     %first part of the path
+    setup.expt_date         =   [currentInfo{i,5}];     %part of the path, YYYY-MM-DD
+    setup.ROI               =   [currentInfo{i,6}];     %which data to consider as coming from the same ROI, per mouse
+    setup.imaging_set       =   [currentInfo{i,7}];     %block or BOT numbers
+    setup.Tosca_session     =   [currentInfo{i,8}];     %Tosca session
+    setup.Tosca_run         =   [currentInfo{i,9}];     %Tosca run
+    setup.analysis_name     =   [currentInfo{i,10}];    %part of the path, folder where fall.mats are stored
+    setup.framerate         =   [currentInfo{i,11}];    %15 or 30, eventually we can detect this automatically
+    setup.run_redcell       =   [currentInfo{i,12}];    %do you have red cells? 0 or 1
+    setup.voltage_recording =   [currentInfo{i,13}];    %0 for widefield, 1 for 2p
+    setup.stim_name         =   [currentInfo{i,14}];    %type of stim presentation in plain text
+    setup.stim_protocol     =   [currentInfo{i,15}];    %number corresponding to stim protocol
 
     setup.block_name = strcat('Compiled_', setup.mousename, '_', setup.expt_date, ...
         '_Block_', num2str(setup.imaging_set), '_Session_', num2str(setup.Tosca_session), ...
         '_Run_', num2str(setup.Tosca_run), '_', setup.stim_name);
     
-    suite2p_path = strcat(setup.pathname, '/', setup.mousename, '/', setup.analysis_name);
-    Tosca_path = strcat(setup.pathname, '/', setup.mousename, '/Tosca_', setup.mousename);
+    %Not every user has a username folder, allow for this column to be empty
+    if ~isempty(setup.username)
+        usernameSlash = strcat(setup.username, '/');
+    else
+        usernameSlash = '';
+    end
+    
+    suite2p_path = strcat(setup.pathname, '/', usernameSlash, setup.mousename, '/', setup.analysis_name);
+    Tosca_path   = strcat(setup.pathname, '/', usernameSlash, setup.mousename, '/Tosca_', setup.mousename);
     
     block = compile_block(setup, suite2p_path, Tosca_path);
 
     %Optionally visually check block
-    if verify == 1
-        verify_block(block);
+    if visualize == 1
+        visualize_block(block);
     end
     
     cd(save_path)

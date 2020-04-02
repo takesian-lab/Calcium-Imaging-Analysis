693b6a5d8e50c21fbb4a96d92e99d190be5c252e
carolyngs <53016415+carolyngs@users.noreply.github.com>
2020-04-02 15:03:04 -0400

    Update FreqDisc_Behavior.m

--- FreqDisc_Behavior.m
+++ FreqDisc_Behavior.m
@@ -1,32 +1,60 @@
 
-% Experiment mice 
-% mouseID='VxAC031419M1';
-% sid='Session 25';
-% folder = sprintf(['D:/2P analysis/2P local data/Carolyn/' mouseID '/Tosca_' mouseID '/'  sid]);
-% boi=[2:9];%blocks of interest (which ones are we actually analyzing)
-% Day=[('27')];
-%  
-
-%something about stimtype=7......
+%%setup.stim_protocol=7;
 Hit_threshold = 0.5;
 Ignore_prep_trial = 1;
 by_date=1;
 
-%operant tone daily prep does not contain CS- trials. screen out trials
-%that do not have CS- trials.
+%% find prep trials
+findPrep = block.trialType;
+r = ismember(0, findPrep);
+if r ==0
+    block.prepTrial = 1;
+else block.prepTrial = 0;
+end
 
-%determine %hit so that we can only look at blocks that are at teh
-%Hit_threshold
- Hits = find(cell2mat(block.b_Outcome)==1);
- FA = find(cell2mat(block.b_Outcome)==4);
- Total = length(block.b_Outcome);
- perHit = length(Hits)/Total;
- 
- %blocks of interest are ones that meet all of these criteria - for a given
- %mouse? for a given day? 
- 
- 
+%% find hitrate; determine if it is above threshold
+Hits = find(block.Outcome==1);
+FA = find(block.Outcome==4);
+HitRate = length(Hits)./length(block.Outcome);
+FARate = length(FA)./length(block.Outcome);
 
+if HitRate >= Hit_threshold
+    includeBlock = 1;
+else includeBlock = 0;
+end
+%% visualize the data?
+%set sound time to zero
+sound_time = block.New_sound_times;
+Trial_time = block.Tosca_times;
+for i = 1:length(Trial_time)
+    center2sound{i} = Trial_time{1,i}(:) - sound_time(i);
+    centerLoc (i) =  find(center2sound{i}==0); 
+    lickWindow(i,:) = (centerLoc(i)-35):(centerLoc(i)+1000);
+    centeredLicks(i,:) = block.lick_time{i,1}(1,lickWindow(1,:));
+end
+
+%cs+ vs cs-
+CSplus = find(block.trialType==1);
+CSminus = find(block.trialType==0);
+for i = 1:length(CSplus)
+    CSplusLicks(i,:) = centeredLicks(CSplus(i),:);
+end
+for i = 1:length(CSminus)
+    CSminusLicks(i,:) = centeredLicks(CSminus(i),:);
+end
+
+timescale = center2sound{1,1}(lickWindow(1,:),1);
+
+subplot(2,1,1)
+imagesc(CSplusLicks)
+title('Licks aligned to Sound, CS+')
+
+
+subplot(2,1,2)
+imagesc(CSminusLicks)
+title('Licks aligned to Sound, CS-')
+
+   
 %% Analyze session - this will be where FreqDisc will actually start!
 session=[]; 
 count=1;

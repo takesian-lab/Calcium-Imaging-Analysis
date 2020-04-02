aabbacaa74947f78124bbf01b73b7e0778a4ae62
carolyngs <53016415+carolyngs@users.noreply.github.com>
2020-04-01 22:01:15 -0400

    Update define_suite2p_singleblock.m
    removing sdmov because I dont have that variable..

--- define_suite2p_singleblock.m
+++ define_suite2p_singleblock.m
@@ -38,7 +38,7 @@ block.img.refImg = Fall.ops.refImg;
 block.img.max_proj = Fall.ops.max_proj;
 block.img.meanImgE = Fall.ops.meanImgE;
 block.img.Vcorr = Fall.ops.Vcorr;
-block.img.sdmov = Fall.ops.sdmov;
+% block.img.sdmov = Fall.ops.sdmov;
 
 block.stat = Fall.stat;
 block.F = Fall.F(:,Frame_set);

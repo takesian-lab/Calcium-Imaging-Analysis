35c34a2706c946a7d5f2ada0e324a34310f343a8
carolyngs <53016415+carolyngs@users.noreply.github.com>
2020-05-12 10:48:25 -0400

    Update align_to_stim.m

--- align_to_stim.m
+++ align_to_stim.m
@@ -1,4 +1,4 @@
-function [block] = align_to_stim(block)
+function [block] = align_to_stim(block,m)
 % DOCUMENTATION IN PROGRESS
 % 
 % This function pulls out the trial windows for each sound presentation
@@ -6,20 +6,25 @@ function [block] = align_to_stim(block)
 % 
 % Argument(s): 
 %   block (struct)
+%   m (struct)
 % 
-% Returns:
+% Returns: 
 %   block (struct)
 % 
 % Notes:
+%
+%
+% 
 %
 % Variables needed from block.setup:
 % -Sound_Time - from define_sound
 % -timestamp - from define_sound
 % -F - from define_suite2p
 % -Fneu - from define_suite2p
+% - spks - from define_suite2p
 %
 %
-% TODO: Magic numbers, also pull out neuropil and spike traces
+% TODO: move dF/F calculation to analysis_2P_v3
 % Search 'TODO'
 
 %%  Skip this function if Suite2p and Bruker data are not available
@@ -31,28 +36,41 @@ end
 
 disp('Aligning to stim...');
 
-%% Pull out the sound traces to each noiseburst
+%% Pull out the traces to each stim
 %define sound window
 Sound_Time = block.Sound_Time;
+
+% loop through each stim-presenation
 for time=1:length(Sound_Time)
     sound = Sound_Time(time);
-    before = Sound_Time(time)-0.5; % half second before sound
-    after = Sound_Time(time)+2.5; % full trace after sound
-    window = Sound_Time(time)+1; % when is the sound?
-    start_window = Sound_Time(time)+1;
     
+    % define when baseline starts
+    before = Sound_Time(time)-m.baseline_length;
     
+    % define the end time of the trace
+    after = Sound_Time(time)+m.after_stim;
+    
+    %define the "response window" (stim presentation to end of expected
+    %trace)
+    window = Sound_Time(time)+m.response_window; 
+
+    
+    % find the frames that correspond to the above times. This will give
+    % you four frame numbers per stim. 1) start baseline 2) stim 3) end of
+    % the response window 4) end of the trace
     [c closest_frame_before] = min(abs(block.timestamp(:)-before));
-    [c closest_frame_start_window] = min(abs(block.timestamp(:)-start_window));
     [c closest_frame_sound] = min(abs(block.timestamp(:)-sound));
     [c closest_frame_after] = min(abs(block.timestamp(:)-after));
     [c closest_frame_window] = min(abs(block.timestamp(:)-window));
     
-    
+    % how long is the entire trace (in frames)?
     length_sound_trial(time) = closest_frame_after-closest_frame_before;
+    % how long is the response window (in frames)?
     length_sound_window(time) = closest_frame_window-closest_frame_sound;
     
-    %     if time*a>1
+   % sometimes, the length of the trials are off by one frame due to how we
+   % calculate the frame times. Below, we correct for this by making every
+   % trace the same as time=1
     if time>1
         difference_length_sound_trial(time) = length_sound_trial(time)-length_sound_trial_first;
         closest_frame_after = closest_frame_after-difference_length_sound_trial(time);
@@ -63,17 +81,32 @@ for time=1:length(Sound_Time)
         length_sound_trial_first = length_sound_trial(time);
     end
     
+    % loop through each "iscell" to find the stim-aligned 1) raw
+    % fluoresence 2) neuropil signal 3) neuropil-corrected floresence 4)
+    % df/F for the neuropil corrected fluoresence 5) deconvolved spikes
     for k = 1:size(block.F,1)
         %generate a neuropil corrected trace 
-        F7 = block.F(k,:) - 0.7*block.Fneu(k,:);
-        % pull out the frames aligned to a stim (0.5s before to 3s after)
-        raw_trace(k,time,:) = (F7(closest_frame_before:closest_frame_after));
+        % neuropil corrected trace = raw F - (neuropil
+        % coefficient)*neuropil signal
+        F7 = block.F(k,:) - m.neucoeff*block.Fneu(k,:);
+        
+        % pull out the frames aligned to a stim (defined in frames)
+        F7_stim(k,time,:) = (F7(closest_frame_before:closest_frame_after));
+        F_stim(k,time,:) =  block.F(k,closest_frame_before:closest_frame_after);
+        Fneu_stim(k,time,:) = block.Fneu(k,closest_frame_before:closest_frame_after);
+        spks_stim(k,time,:) = block.spks(k,closest_frame_before:closest_frame_after);
+        
         %df/f
-        baseline_mean = mean(F7(closest_frame_before:closest_frame_sound));
-        trace_around_stim(k,time,:) = (bsxfun(@minus, raw_trace(k,time,:),baseline_mean))./baseline_mean;
+%         baseline_mean = mean(F7(closest_frame_before:closest_frame_sound));
+%         F7_dfF_stim(k,time,:) = (bsxfun(@minus, raw_trace(k,time,:),baseline_mean))./baseline_mean;
     end
 end
- block.aligned_to_stim = trace_around_stim;
+%  block.aligned_stim.F7_dfF_stim = F7_dfF_stim;
+ block.aligned_stim.F7_stim = F7_stim;
+ block.aligned_stim.F_stim = F_stim;
+ block.aligned_stim.Fneu_stim = Fneu_stim;
+ block.aligned_stim.spks_stim = spks_stim;
+ 
 end
 
 

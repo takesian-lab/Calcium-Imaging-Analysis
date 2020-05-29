956b9ebf412e2c632a97f4db6208d1d30ae76ecc
carolyngs <53016415+carolyngs@users.noreply.github.com>
2020-05-28 21:29:07 -0400

    Update plot_loco_by_stim.m

--- plot_loco_by_stim.m
+++ plot_loco_by_stim.m
@@ -1,3 +1,4 @@
+function [data] = plot_loco_by_stim(data);
 setup = data.setup;
 
 for a=1:size(setup.mousename,1)
@@ -15,38 +16,35 @@ for a=1:size(setup.mousename,1)
         noLoc_trial = ~data.([mouseID]).cat.isLoco_cat;
         
         % active trials
-        for i=1:size(stimdata.F7_df_f,1)
-            loc_avg(i,:,:) = mean(stimdata.F7_df_f(:,Loc_trial,:),2); %Active trials
-        end
-        
-        loc_trace = mean(loc_avg,1);
-        loc_sem = std(loc_avg)./sqrt(size(loc_avg,1));
+        loc_data(:,:,:) = stimdata.F7_df_f(:,Loc_trial,:);
+        loc_trace(:,:) = squeeze(mean(loc_data,2)); %Active trials
+        loc_sem = std(loc_trace)./sqrt(size(loc_trace,1));
+        loc_avg = mean(loc_trace,1);
         
         
         %non-active trials
-        for i=1:size(stimdata.F7_df_f,1)
-            noloc_avg(i) = mean(stimdata.F7_df_f(:,noLoc_trial,:),2); %Active trials
-        end
-        
-        noloc_trace = mean(noloc_avg,1);
-        noloc_sem = std(noloc_avg)./sqrt(size(noloc_avg,1));
+        noloc_data(:,:,:) = stimdata.F7_df_f(:,noLoc_trial,:);
+        noloc_trace(:,:) = squeeze(mean(noloc_data,2)); %Active trials
+        noloc_sem = std(noloc_trace)./sqrt(size(noloc_trace,1));
+        noloc_avg = mean(noloc_trace,1);
         
         
-        x=1:length(loc_trace);
+        x=1:length(loc_avg);
         
         figure
         
         subplot(2,1,1); hold on
         title([mouseID ' FOV ' num2str(FOV)])
-        shadedErrorBar(x,smooth(loc_trace,5),smooth(loc_sem,5),'lineprops','-b','transparent',1);
+        shadedErrorBar(x,smooth(loc_avg,5),smooth(loc_sem,5),'lineprops','-b','transparent',1);
         legend({'Active trials'})
         xlabel('Frames')
         ylabel('Delta F/F')
         
         subplot(2,1,2); hold on
-        shadedErrorBar(x,smooth(noloc_trace,5),smooth(noloc_trace,5),'lineprops','-k','transparent',1);
+        shadedErrorBar(x,smooth(noloc_avg,5),smooth(noloc_avg,5),'lineprops','-k','transparent',1);
         legend({'Inactive trials'})
         xlabel('Frames')
         ylabel('Delta F/F')
     end
+end
 end

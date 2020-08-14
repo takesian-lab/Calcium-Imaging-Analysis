9b6a23ed80d0abb3102d1aa72e3618e8220fdb0f
marysethomas <marysethomas06@gmail.com>
2020-08-13 20:09:37 -0400

    Update plot_behavior_maryse_and_esther.m

--- plot_behavior_maryse_and_esther.m
+++ plot_behavior_maryse_and_esther.m
@@ -1,10 +1,12 @@
 %plot_behavior_maryse_and_esther
 
-%% Get relevant data from block
+%% Get information from block
 
 %Block data
-mousename = block.setup.mousename;
-expt_date = block.setup.expt_date;
+mousename = char(block.setup.mousename);
+expt_date = char(block.setup.expt_date);
+dB_level = block.stim_level;
+plotTitle = strjoin({mousename, expt_date, num2str(dB_level), 'dB'});
 
 %Stimulus data
 allFrequencies = block.TargetFreq;
@@ -26,7 +28,7 @@ raw_reactionTimes(raw_reactionTimes < 0) = nan; %trials with no responses become
 raw_reactionTimes = raw_reactionTimes/1000; %convert to seconds
 reaction_times = raw_reactionTimes - holdingPeriod; %subtract holding period
 
-%% Plot 1 - Psychometric curve
+%% Plot 1 - Psychometric curve (Maryse)
 
 %Calculate hit rate per frequency
 hitsPerFrequency = nan(1,length(uniqueFrequencies)); %make empty vector to fill with data
@@ -43,13 +45,24 @@ y = hitRatePerFrequency;
 %make figure
 figure; hold on
 plot(x,y,'Linewidth',2) %plot psychometric curve
-hline(0.5,'r') %plot horizontal red line at 0.5
+hline(0.5,'r') %plot horizontal red line at 50% hit rate (chance)
 ylabel('Hit Rate')
 xlabel('Alternating frequency')
 set(gca, 'XTick', x)
 set(gca, 'XTickLabel', uniqueFrequencies)
-title([mousename ' ' expt_date])
+title(plotTitle)
 
-%% Plot 2 - 
+%Maryse To Do:
+%1. plot psychometric curve with adjustments for trials where the lick was too early
+%2. plot S-shaped curve over psychometric curve
 
+%% Plot 2 - Reaction time vs. frequency (Esther)
+
+%Calculate average reaction time per frequency
+
+
+%set up axes
+
+
+%make figure
 

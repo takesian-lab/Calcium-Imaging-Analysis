911167d67e2581418ac1c72753b92ed5dfea37e3
marysethomas <marysethomas06@gmail.com>
2020-08-17 16:48:47 -0400

    Update plot_behavior_maryse_and_esther.m

--- plot_behavior_maryse_and_esther.m
+++ plot_behavior_maryse_and_esther.m
@@ -44,8 +44,9 @@ y = hitRatePerFrequency;
 
 %make figure
 figure; hold on
+subplot(2,1,1)
 plot(x,y,'Linewidth',2) %plot psychometric curve
-hline(0.5,'r') %plot horizontal red line at 50% hit rate (chance)
+line([0 x(end)+1], [0.5, 0.5], 'Color', 'r') %plot horizontal red line at 50% hit rate (chance)
 ylabel('Hit Rate')
 xlabel('Alternating frequency')
 set(gca, 'XTick', x)
@@ -59,10 +60,37 @@ title(plotTitle)
 %% Plot 2 - Reaction time vs. frequency (Esther)
 
 %Calculate average reaction time per frequency
-
+reaction_timesPerFrequency_STD = nan(1,length(uniqueFrequencies));
+reaction_timesPerFrequency = nan(1,length(uniqueFrequencies)); %make empty vector to fill with data
+for i = 1:length(uniqueFrequencies)
+    freqResponse = reaction_times(allFrequencies == uniqueFrequencies(i));
+    reaction_timesPerFrequency(i) = nanmean(freqResponse);
+    reaction_timesPerFrequency_STD(i) = nanstd(freqResponse);
+end
+%reaction_timesPerFrequency = reaction_timesPerFrequency./nRepsPerFrequency;
 
 %set up axes
-
+x = 1:length(reaction_timesPerFrequency);
+y = reaction_timesPerFrequency;
+upper = y + reaction_timesPerFrequency_STD;
+lower = y - reaction_timesPerFrequency_STD;
 
 %make figure
+%figure; 
+subplot(2,1,2); hold on
+bar(x,y)
+%errorbar(x,y',lower,upper) %plot psychometric curve
 
+scatterx = nan(1,length(reaction_times));
+for i = 1:length(uniqueFrequencies)
+    scatterx(allFrequencies == uniqueFrequencies(i)) = i;
+end
+scatter(scatterx,reaction_times, 'k')
+
+%line([0 x(end)+1], [0.5, 0.5], 'Color', 'r') %plot horizontal red line at 50% hit rate (chance)
+ylabel('Reaction Time')
+xlabel('Alternating frequency')
+set(gca, 'XTick', x)
+set(gca, 'XTickLabel', uniqueFrequencies)
+title(plotTitle)
+xlim([x(1)-1 x(end)+1])

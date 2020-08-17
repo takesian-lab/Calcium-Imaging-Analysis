7ebc11c973fd7f1bf2dce1d52a11df61ec37a0c1
carolyngs <53016415+carolyngs@users.noreply.github.com>
2020-08-17 12:41:52 -0400

    Update define_loco_singleblock.m

--- define_loco_singleblock.m
+++ define_loco_singleblock.m
@@ -36,35 +36,36 @@ setup = block.setup;
 Sound_Time = block.Sound_Time;
 loco_data = block.locomotion_data; %TRANSFORMED LOCO DATA
 active_time = block.active_time;
+trial_data = block.loc_Trial_times;
 
 %% Find sound times when animal is active
 for time=1:length(Sound_Time)
     sound = Sound_Time(time);% when is the sound?
     window = Sound_Time(time)+setup.constant.locowindow; 
 
-    [c closest_frame_sound] = min(abs(loco_data(:,1)-sound));
-    [c closest_frame_window] = min(abs(loco_data(:,1)-window));
+    [c closest_loc_sound] = min(abs(trial_data{time}(:)-sound));
+    [c closest_loc_window] = min(abs(trial_data{time}(:)-window));
 
-    length_sound_window(time) = closest_frame_window-closest_frame_sound;
+    length_sound_window(time) = closest_loc_window-closest_loc_sound;
 
      if time>1
         difference_length_window(time) = length_sound_window(time)-length_sound_window(1);
-        closest_frame_window = closest_frame_window-difference_length_window(time);
+        closest_loc_window = closest_loc_window-difference_length_window(time);
      end
 
-     closest_frame_sound_all(time) = closest_frame_sound;
-     closest_frame_window_all(time) = closest_frame_window;
+     closest_loc_sound_all(time) = closest_loc_sound;
+     closest_loc_window_all(time) = closest_loc_window;
      
      % if any times are active, it will count as loco time
     
      %For the case where the closest frame surpasses the length of active_time
      %This should only be the case if we're on the last trial, so let the
      %code throw an error if we're not (no else case)
-     if closest_frame_window > length(active_time) && time == length(Sound_Time)
-         closest_frame_window = length(active_time);
+     if closest_loc_window > length(active_time) && time == length(Sound_Time)
+         closest_loc_window = length(active_time);
      end
     
-     Loco_1(time) = sum(active_time(closest_frame_sound:closest_frame_window))>1;
+     Loco_1(time) = sum(active_time(closest_loc_sound:closest_loc_window))>1;
 end
 
 %% Plot locomotor activity

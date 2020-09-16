4ab1c9e6b94513b6ba54b1e7f1da7da8cc409653
carolyngs <53016415+carolyngs@users.noreply.github.com>
2020-09-16 12:03:19 -0400

    Update analysis_widefield_gcamp.m

--- analysis_widefield_gcamp.m
+++ analysis_widefield_gcamp.m
@@ -53,8 +53,8 @@ else
             info_path = 'Z:\Carolyn\2P Imaging data\VIPvsNDNF_response_stimuli_study\Info Sheets';
             %             compiled_blocks_path = 'D:\2P analysis\2P local data\Carolyn\analyzed\Daily Imaging';
             compiled_blocks_path = 'Z:\Carolyn\2P Imaging data\VIPvsNDNF_response_stimuli_study\Compiled Blocks';
-            save_path = 'Z:\Carolyn\2P Imaging data\VIPvsNDNF_response_stimuli_study\analyzed widefield\VxDD053120M2\gcamp_RF';
-            info_filename = 'Info_VxDD053120M2';
+            save_path = 'Z:\Carolyn\2P Imaging data\VIPvsNDNF_response_stimuli_study\analyzed widefield\NxDC030220F2\gcamp_RF';
+            info_filename = 'Info_NxDC030220F2';
             
         case 'RD-6-TAK2' %Esther's computer
             info_path = 'Z:\Carolyn\2P Imaging data\SSRI study with Jacob';
@@ -93,7 +93,7 @@ end
 % approximately 500ms. Set this value here, to visually test the shift.
 % (the adjust_factor is in seconds)
 adjust_factor = 0.5; % in seconds
-
+ 
 for i=1:length(data.setup.Imaging_sets)
     mouseID=data.setup.mousename{i};
     unique_block_name = data.setup.unique_block_names{i};
@@ -141,7 +141,7 @@ for i=1:length(data.setup.Imaging_sets)
         [c closest_frame_before] = min(abs(timestamp(:,1)-before));
         [c closest_frame_sound] = min(abs(timestamp(:,1)-sound));
         [c closest_frame_after] = min(abs(timestamp(:,1)-after));
-        
+             
         length_sound_trial(y) = closest_frame_after-closest_frame_before;
         
         if y>1
@@ -424,7 +424,7 @@ for f=1:length(parameters.frequencies)
 end
 %% convert to tif? and then store as individual file.
 folder = save_path;
-
+% folder = 'Z:\Carolyn\2P Imaging data\VIPvsNDNF_response_stimuli_study\analyzed widefield\VxDD033120F2\gcamp_RF';
 %folder = 'C:\Anne\';
 cd(folder)
 tic
@@ -442,154 +442,6 @@ for f=1:length(parameters.frequencies);
         end
     end
 end
-%% temporal and spatial denoise- takes approx. 30 hours
-
-% if temp_spat_analysis == 1
-% t=cputime
-%     %folder = 'D:\2P analysis\2P local data\Wisam\YD111219F3\2020-03-06\AnalyzedTiffs\';
-%     folder = 'C:\Anne\';
-%     cd(folder)
-%     d = dir([folder '/*.tif']);%extract tiffs
-%
-%    parfor f=1:length(parameters.frequencies)
-%         numF=num2str(round(parameters.frequencies(f)))
-%         for lv=1:length(parameters.levels)
-%             numLV=num2str(parameters.levels(lv))
-%             idx=parameters.stimIDX{f,lv};
-%             fname = (['avgStim' numF 'khz' numLV 'db.tif']);
-%             info = imfinfo(fname);
-%             num_images = numel(info);
-%             for k = 1:num_images
-%                 AA = imread(fname, k);
-%                 stack(:,:,k)=AA(:,:);
-%                 stack=double(stack);
-%             end
-%             outputFileName= (['TempDenoise' numF 'khz' numLV 'db.tif']);
-%             disp(['Temporally deconvolving for, ' num2str(parameters.frequencies(f))  ' KHz, ' num2str(parameters.levels(lv)) ' dB'])
-%
-%
-%             %             tempD=zeros(size(mm,1),length(y),size(mm,3));
-% %             clear tempD
-%             tempD=zeros(size(stack,1),(size(stack,2)),size(stack,3));
-%             for x=1:size(stack,1);
-%
-%
-%                 for y=1:size(stack,2);
-%                     %temporal filter using Paninski code
-%                     A =(stack(x,y,:));
-%                     [c,b,c1,g,sn,sp]=constrained_foopsi_new(A); %,b,c1,g,sn,options);
-%                     tempD(x,y,:) = c;
-%                 end
-%             end
-%             for kk = 1:size(tempD,3);
-%                 imwrite(tempD(:, :, kk), outputFileName, 'WriteMode', 'append');
-%             end
-%             %
-%         end
-%
-%     end
-% e=t-cputime
-% clear tempD x y stack t sp sn outsputFileName lumLB numF...
-%     num_images lv ll loops loop_num kk k infor info idx g...
-%     e d
-%
-% % spatial deconvolve
-%
-% t=cputime;
-% PSF=fspecial('gaussian',[3 3],0.5);
-% %folder = 'D:\2P analysis\2P local data\Wisam\YD111219F3\2020-03-06\AnalyzedTiffs\';
-% folder = 'C:\Anne\';
-% cd(folder)
-% d = dir([folder '/*.tif']);%extract tiffs
-% parfor f=1:length(parameters.frequencies);
-%     numF=num2str(round(parameters.frequencies(f)));
-%     for lv=1:length(parameters.levels);
-%         numLV=num2str(parameters.levels(lv));
-%         idx=parameters.stimIDX{f,lv};
-%         fname= (['TempDenoise' numF 'khz' numLV 'db.tif'])
-%         info = imfinfo(fname);
-%         num_images = numel(info);
-%         for k = 1:num_images
-%             AA = imread(fname, k);
-%             stack(:,:,k)=AA(:,:);
-%             stack=double(stack);
-%         end
-%         outputFileName= (['SpatDenoise' numF 'khz' numLV 'db.tif']);
-%         disp(['Spatially deconvolving for, ' num2str(parameters.frequencies(f))  ' KHz, ' num2str(parameters.levels(lv)) ' dB'])
-%
-%         spatD=zeros(size(stack,1),(size(stack,2)),size(stack,3));
-%
-%                 for time=1:size(stack,3)
-%                     spatD(:,:,time) = deconvlucy(stack(:,:,time), PSF); %spatial filter for each pixel
-%                 end
-%
-%         for kk = 1:size(spatD,3);
-%             imwrite(spatD(:, :, kk), outputFileName, 'WriteMode', 'append');
-%         end
-%     end
-% end
-% e=t-cputime
-% end
-
-%% plot temporal noise responses
-%folder = 'D:\2P analysis\2P local data\Wisam\YD111219F3\2020-03-06\AnalyzedTiffs\';
-% folder = 'C:\Anne';
-%     cd(folder)
-%     d = dir([folder '/*.tif']);%extract tiffs
-%
-% for f=1:length(parameters.frequencies);
-%     numF=num2str(round(parameters.frequencies(f)));
-%     for lv=1:length(parameters.levels);
-%         numLV=num2str(parameters.levels(lv));
-%
-%         fname1= (['TempDenoise' numF 'khz' numLV 'db.tif'])
-%
-%             info1 = imfinfo(fname1);
-%             num_images1 = numel(info1);
-%             for k1 = 1:num_images1
-%                 AA1 = imread(fname1, k1);
-%                 stack1(:,:,k1)=AA1(:,:);
-%                 tempD_stack=double(stack1);
-%             end
-%
-%         %plot avg stim
-%         mean_dff0=squeeze(mean(mean(avgStim_stack,1),2));
-%         x=1:length(mean_dff0);
-%         figure; plot (x, smooth(mean_dff0,10), '-r'); hold on;
-%
-%
-%         if temp_spat_analysis == 1
-%             fname2 = (['avgStim' numF 'khz' numLV 'db.tif'])
-%             fname3 = (['SpatDenoise' numF 'khz' numLV 'db.tif'])
-%
-%             info2 = imfinfo(fname2);
-%             num_images2 = numel(info2);
-%             for k2 = 1:num_images2
-%                 AA2 = imread(fname2, k2);
-%                 stack2(:,:,k2)=AA2(:,:);
-%                 avgStim_stack=double(stack2);
-%             end
-%
-%             info3 = imfinfo(fname3);
-%             num_images3 = numel(info3);
-%             for k3 = 1:num_images3
-%                 AA3 = imread(fname3, k3);
-%                 stack3(:,:,k3)=AA3(:,:);
-%                 SpatDenoise_stack=double(stack3);
-%             end
-%             mean_temp=squeeze(mean(mean(tempD_stack,1),2));
-%             mean_spat=squeeze(mean(mean(SpatDenoise_stack,1),2));
-%             plot(x, smooth(mean_temp,10), 'b');hold on
-%             plot(x, smooth(mean_spat,10), 'g');
-%
-%             %plot normalized
-%             figure; plot (x, smooth(mean_dff0,10)./max(smooth(mean_dff0,10)), '-r'); hold on;
-%             plot(x, smooth(mean_temp,10)./max(smooth(mean_temp,10)), 'b');hold on
-%             plot(x, smooth(mean_spat,10)./max(smooth(mean_spat,10)), 'g');
-%         end
-%     end
-% end
-
 %% %% find cumulative baseline and the response window
 baseline=1:(0.5*data.setup.FrameRate{1});%TODO magic number
 window=(length(baseline)):(data.setup.FrameRate{1}*3);
@@ -789,7 +641,7 @@ end
 %% determine CFs for each pixel
 [dim1 dim2 dim3] = size(imageData.Cropped_Imaging_Data);
 CF=NaN(dim1,dim2,1);           
-response_threshold = 3;
+response_threshold = 1.5;
 
 
 for x= 1:dim1 % go through all x pixels
@@ -932,3 +784,5 @@ set(im2,'AlphaData',~isnan(CF_mask))
 %     d = datestr(now,'yyyymmdd-HHMMSS');
 %     save(['Data_' d '.mat'], 'data');
 % end
+ cd(save_path)
+ save('imageData.mat','imageData','-v7.3');   

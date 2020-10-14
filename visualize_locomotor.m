dd7b2fed7caede47b8b6badb63eb4823d5d0bf32
carolyngs <53016415+carolyngs@users.noreply.github.com>
2020-10-14 06:09:17 -0400

    Update visualize_locomotor.m

--- visualize_locomotor.m
+++ visualize_locomotor.m
@@ -2,43 +2,43 @@ function [loco_cor_cell] = visualize_locomotor(block)
 % Carolyn 5/29/2020
 
 
-    %% Magic numbers and define what type of analysis you are doing
-    %stim protocol code is:
-    %noiseburst = 1
-    %ReceptiveField = 2
-    %FM sweep = 3
-    %SAM = 6
-    %widefield = 4
-    %SAM freq = 6
-    %Behavior = 7 and 8
-    %Random H20 = 9
-    %Noiseburst_ITI = 10
-    %Random air puff = 11
-    run_redcell = 0;
-    std_level = 1.5;
-    std_level_byStim = 1.5;
+%% Magic numbers and define what type of analysis you are doing
+%stim protocol code is:
+%noiseburst = 1
+%ReceptiveField = 2
+%FM sweep = 3
+%SAM = 6
+%widefield = 4
+%SAM freq = 6
+%Behavior = 7 and 8
+%Random H20 = 9
+%Noiseburst_ITI = 10
+%Random air puff = 11
+run_redcell = 0;
+std_level = 1.5;
+std_level_byStim = 1.5;
 
 
 
 %% loco_trace
-    
+
 %DF/F for fluorescent trace
-            F = block.F; %fluoresence
-            Fneu = block.Fneu; % neuropil
-            F7 = F - block.setup.constant.neucoeff * Fneu; % neuropil corrected trace
-            
-            for k = 1:size(F7,1)
-                baseline_Fo = mean(F7(k,:),2);
-                DF_F0(k,:) = F7(k,:)-baseline_Fo./baseline_Fo;
-             locdetrend_temp = locdetrend(F7(k,:),1,[300 10]);
-             DF_F0_dt(k,:) = locdetrend_temp'./(F7(k,:)-locdetrend_temp');
-%              DF_F0_dt(k,:) = F7(k,:);
-            end
-            
-            locoTrace = block.loco_activity;
-            locoTime = block.loco_times;
-            redcell = block.redcell;
-           
+F = block.F; %fluoresence
+Fneu = block.Fneu; % neuropil
+F7 = F - block.setup.constant.neucoeff * Fneu; % neuropil corrected trace
+
+for k = 1:size(F7,1)
+    baseline_Fo = mean(F7(k,:),2);
+    DF_F0(k,:) = F7(k,:)-baseline_Fo./baseline_Fo;
+    locdetrend_temp = locdetrend(F7(k,:),1,[300 10]);
+    DF_F0_dt(k,:) = locdetrend_temp'./(F7(k,:)-locdetrend_temp');
+    %              DF_F0_dt(k,:) = F7(k,:);
+end
+
+locoTrace = block.loco_activity;
+locoTime = block.loco_times;
+redcell = block.redcell;
+
 
 
 %% plot detrended data...(ten traces)....
@@ -71,7 +71,7 @@ end
 
 %% upsample loco trace
 % remove NaNs by interpolation
-% paint = inpaint_nans(locoTrace); 
+% paint = inpaint_nans(locoTrace);
 
 % upsample by interplation to make same size as Ca++ trace
 v = 1:numel(locoTrace);
@@ -91,11 +91,11 @@ locoTrace_up(locoTrace_up<block.setup.constant.locoThresh)=0;
 % for i =1;%:20;%size(DF_F0,1)
 %     y = DF_F0_loco(i,:)+count;
 %     plot(smooth(y,10)); hold on
-%     count = count+3;    
+%     count = count+3;
 % end
 % subplot(2,1,2)
 %      plot(smooth(locoTrace_up(1,:),10))
-    
+
 %% Plot locomotor trace (now, thresholded), against gcamp signal (detrended)
 % Make the first axes in its figure
 % figure;
@@ -103,28 +103,28 @@ locoTrace_up(locoTrace_up<block.setup.constant.locoThresh)=0;
 % x2 = 1:numel(locoTrace_up);
 % for i = 1:size(DF_F0_loco,1)
 %     y= DF_F0_loco(i,:);
-%   
+%
 %     if redcell(i) ==1
 %          subplot(8, 5, i);
-%    
+%
 % %     scatter(x,y,'ro');
 %     mygca(i) = gca;
 % %     yb = scatstat1(x,y,15,@median);
 % %     plot(x,yb,'ro')
-% % x = 1:50; 
-% % y = -0.3*x + 2*randn(1,50); 
-% p = polyfit(x,y,1); 
-% f = polyval(p,x); 
-% plot(x,y,'o',x,f,'-') 
-% legend('data','linear fit') 
+% % x = 1:50;
+% % y = -0.3*x + 2*randn(1,50);
+% p = polyfit(x,y,1);
+% f = polyval(p,x);
+% plot(x,y,'o',x,f,'-')
+% legend('data','linear fit')
 %     else
 %          hAx(i)=subplot(8, 8, i);
 % %          scatter(x,y,'go');
 %            mygca(i) = gca;
-%            p = polyfit(x,y,1); 
-% f = polyval(p,x); 
-% plot(x,y,'o',x,f,'-') 
-% legend('data','linear fit') 
+%            p = polyfit(x,y,1);
+% f = polyval(p,x);
+% plot(x,y,'o',x,f,'-')
+% legend('data','linear fit')
 %     end
 % end
 % yl = cell2mat(get(mygca, 'Ylim'))
@@ -133,24 +133,24 @@ locoTrace_up(locoTrace_up<block.setup.constant.locoThresh)=0;
 
 % scatterhist(locoTrace_up,DF_F0_loco(1,:));
 
-%% 
+%%
 % % find data associated with running versus data associated with not running
-% 
+%
 % noloco_boutIDX =locoTrace_up==0;
 % loco_boutIDX = ~noloco_boutIDX;
-% 
+%
 % for i = 1:size(DF_F0_loco,1)
 %     mean_loco(i) = mean(DF_F0_loco(i,loco_boutIDX));
 %     mean_noloco(i) =mean(DF_F0_loco(i,noloco_boutIDX));
 % end
-% 
+%
 % mLoco = mean(mean_loco);
 % semLoco = std(mean_loco)./sqrt(numel(mean_loco));
-% 
-% 
+%
+%
 % % scatter(x,mean_loco); hold on
 % % scatter(x2, mean_noloco)
-% 
+%
 % mnoLoco =  mean(mean_noloco);
 % semnoLoco = std(mean_noloco)./sqrt(numel(mean_noloco));
 %% do the correlation  - not actually z-scored, I just didnt change the name of the variables
@@ -163,38 +163,38 @@ z_locoDiff = zscore(diff(locoTrace_up));
 
 count = 1;
 for i = 1:size(DF_F0_loco,1)
-%     if redcell(i)==1
-z_DF_F0(i,:) = (DF_F0_loco(i,:));
-[coef,lags] = xcorr(z_DF_F0(i,:),z_locoTrace(:,:),'coeff',60); %magic number
-% [coef_diff,lags_diff] = xcorr(z_locoDiff,z_DF_F0(i,:));
-[max_cor,idx] = max(coef);
-cor_idx(i) = idx;
-% [max_cor_diff,idx_diff] = max(coef);
-max_c(i) = max_cor;
-% max_c_diff(i) = max_cor_diff;
-max_lag(i) = lags(idx);
-[r,p] = corrcoef(z_DF_F0(i,:), z_locoTrace(:,:));
-pval{i}=p;
-if pval{i}(1,2)<0.05
-    loco_cor_cell(count) = block.cell_number(i)
-    count = count+1;
-end
-
-rr(i,:)=r(1,2);
-% pp(i,:)=p(1,2)
-
-figure;
-
-
-plot(lags,coef,'-b')
-title(['cell number ',num2str(block.cell_number(i))])
-% max_lag_diff = lags_diff(idx_diff);
-%     end
+    %     if redcell(i)==1
+    z_DF_F0(i,:) = (DF_F0_loco(i,:));
+    [coef,lags] = xcorr(z_DF_F0(i,:),z_locoTrace(:,:),'coeff',60); %magic number
+    % [coef_diff,lags_diff] = xcorr(z_locoDiff,z_DF_F0(i,:));
+    [max_cor,idx] = max(coef);
+    cor_idx(i) = idx;
+    % [max_cor_diff,idx_diff] = max(coef);
+    max_c(i) = max_cor;
+    % max_c_diff(i) = max_cor_diff;
+    max_lag(i) = lags(idx);
+    [r,p] = corrcoef(z_DF_F0(i,:), z_locoTrace(:,:));
+    pval{i}=p;
+    if pval{i}(1,2)<0.05
+        loco_cor_cell(count) = block.cell_number(i)
+        count = count+1;
+    end
+    
+    rr(i,:)=r(1,2);
+    % pp(i,:)=p(1,2)
+    
+    figure;
+    
+    
+    plot(lags,coef,'-b')
+    title(['cell number ',num2str(block.cell_number(i))])
+    % max_lag_diff = lags_diff(idx_diff);
+    %     end
 end
 %% trace in response to loco onset/offset
 % loco_trace_filt = smooth(locoTrace_up,10);
 % for i = 2:length(loco_trace_filt)
-%     if loco_trace_filt(i-1) ==0 && loco_trace_filt(i)>0 
+%     if loco_trace_filt(i-1) ==0 && loco_trace_filt(i)>0
 %         onsetIDX(i)=1;
 %     end
 %     if loco_trace_filt(i-1)>0 && loco_trace_filt(i)==0
@@ -217,7 +217,7 @@ end
 %         end
 %        x=1:size(onFrames(i)-pre_onset:onset_end+onFrames(i),2);
 %     end
-%      
+%
 %         a_trace = mean(on_trace_wind,1);
 %         onset_traces(k,:) = a_trace;
 %         plot(x,smooth(onset_traces(k,:),10));hold on
@@ -227,12 +227,12 @@ end
 % % mean_onset = mean(on_trace_wind,1);
 % % sem_onset = std(on_trace_wind,1)./sqrt(size(on_trace_wind,1));
 % % shadedErrorBar(x,smooth(mean_onset,10),smooth(sem_onset,10),'lineProps','-b');
-% 
+%
 % % plot(mean_onset)
-% 
+%
 % %% find mean loco onset traces
-% 
-% 
+%
+%
 % %%
 % figure;
 % scatter(lags,coef)
@@ -245,10 +245,10 @@ base_frames = stimTrace(:,:,1:baseline);
 mean_base_frames = mean(base_frames,3);
 for i = 1:size(stimTrace,1)
     for j = 1:size(stimTrace,2)
-       ftemp = stimTrace(i,j,:);
-       btemp = mean_base_frames(i,j);
-       dftemp = ftemp-btemp./btemp;
-       df_f(i,j,:) = dftemp;
+        ftemp = stimTrace(i,j,:);
+        btemp = mean_base_frames(i,j);
+        dftemp = (ftemp-btemp)/btemp;
+        df_f(i,j,:) = dftemp;
     end
 end
 
@@ -259,21 +259,42 @@ active_traces = df_f(:,acttrial,:);
 inactive_traces = df_f(:,inacttrial,:);
 
 %avg across cells
-for i = 1:size(active_traces,1) 
+for i = 1:size(df_f,1)
     a = squeeze(mean(active_traces(i,:,:),2));
     a_err = squeeze(std(active_traces(i,:,:),0,2)/sqrt(size(active_traces,2)));
     b = squeeze(mean(inactive_traces(i,:,:),2));
     b_err = squeeze(std(inactive_traces(i,:,:),0,2)/sqrt(size(inactive_traces,2)));
     x = 1:size(active_traces,3);
-    figure; 
-    shadedErrorBar(x,smooth(a,10),a_err,'lineprops','-g'); hold on
-    shadedErrorBar(x,smooth(b,10),b_err,'lineprops','-m'); hold on
+    
+    
+    figure;
+    subplot(3,1,1)
+    shadedErrorBar(x,smooth(a,10),smooth(a_err,5),'lineprops','-g'); hold on
+    shadedErrorBar(x,smooth(b,10),smooth(b_err,5),'lineprops','-m'); hold on
     vline(baseline)
     
-    active_cell(i,:,:) =a; 
+    active_cell(i,:,:) =a;
     inactive_cell(i,:,:) = b;
     title(['cell number ',num2str(block.cell_number(i))])
     legend('active','inactive')
+    
+    subplot(3,1,2)
+    imagesc(squeeze(active_traces(i,:,:)))
+    vline(baseline)
+    %         set(gca, 'XTick', x_in_seconds)
+    %         set(gca, 'XTickLabel', x_label_in_seconds)
+    xlabel('frames')
+    xlim([0 size(active_traces,3)])
+    ylabel('Trials')
+    
+    subplot(3,1,3)
+    imagesc(squeeze(inactive_traces(i,:,:)))
+    vline(baseline)
+    %         set(gca, 'XTick', x_in_seconds)
+    %         set(gca, 'XTickLabel', x_label_in_seconds)
+    xlabel('frames')
+    xlim([0 size(active_traces,3)])
+    ylabel('Trials')
 end
 
 

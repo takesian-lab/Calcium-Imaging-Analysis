b9d038e9c16fe86bf60a0f70425745a802893907
carolyngs <53016415+carolyngs@users.noreply.github.com>
2021-05-13 16:35:32 -0400

    Update create_valid_varname.m

--- Tosca functions/create_valid_varname.m
+++ Tosca functions/create_valid_varname.m
@@ -2,6 +2,7 @@ function varName = create_valid_varname(str)
 
 varName = deblank(str);
 varName = strrep(varName, ' ', '_');
+varName = strrep(varName, '---.', '');
 varName = strrep(varName, '-', '_minus');
 varName = strrep(varName, '(', '');
 varName = strrep(varName, ')', '');
@@ -12,6 +13,8 @@ varName = strrep(varName, '?', '');
 varName = strrep(varName, '+', '_plus');
 varName = strrep(varName, '#', 'num');
 varName = strrep(varName, ':', '.');
+varName = strrep(varName, '<', '');
+varName = strrep(varName, '>', '');
 varName = strrep(varName, char(181), 'u');
 if varName(1)>='0' && varName(1)<='9',
     varName = ['Key_' varName];

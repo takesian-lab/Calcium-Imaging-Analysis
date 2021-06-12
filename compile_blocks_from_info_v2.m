16c469d170a7c0876e4a8192d66393c44d021580
marysethomas <marysethomas06@gmail.com>
2021-06-12 16:00:40 -0400

    Update compile_blocks_from_info_v2.m

--- compile_blocks_from_info_v2.m
+++ compile_blocks_from_info_v2.m
@@ -58,9 +58,9 @@ switch PC_name
     case 'RD0366' %Maryse
         info_path = 'D:\Data\2p\VIPvsNDNF_response_stimuli_study';
         save_path = 'D:\Data\2p\VIPvsNDNF_response_stimuli_study\CompiledBlocks_V2';
-        info_path = '\\apollo\research\ENT\Takesian Lab\Maryse\2p data\Behavior';
-        save_path = '\\apollo\research\ENT\Takesian Lab\Maryse\2p data\Behavior\Compiled Blocks v2';
-        info_filename = 'Info v2';
+        %info_path = '\\apollo\research\ENT\Takesian Lab\Maryse\2p data\Behavior';
+        %save_path = '\\apollo\research\ENT\Takesian Lab\Maryse\2p data\Behavior\Compiled Blocks v2';
+        info_filename = 'Info_NxDG021621F2';
         ops_filename = 'Maryse_ops2.mat';
          
     case 'TAKESIANLAB2P' %2P computer
